c1bd5be6354a19a8cc1ab20fb689f204b195ce0b
ima17 <ima.sudaraka@outlook.com>
2023-04-10 01:21:36 +0530

    Request permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:ar_smart_whiteboard/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.camera.request();
+  await Permission.sensors.request();
+
   runApp(const MyApp());
 }
 
