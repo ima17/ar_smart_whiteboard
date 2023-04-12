bac67865c70eb9ca1a07c44cc6233c56ff623528
ima17 <ima.sudaraka@outlook.com>
2023-04-12 22:48:18 +0530

    Remove loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:ar_smart_whiteboard/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'screens/loading_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Future.wait([
-    Permission.camera.request(),
-    Permission.sensors.request(),
-    Permission.activityRecognition.request(),
-  ]);
+  await Permission.camera.request();
+  await Permission.sensors.request();
+  await Permission.activityRecognition.request();
 
   runApp(const MyApp());
 }
@@ -22,24 +19,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'AR Smart Whiteboard',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FutureBuilder(
-        future: initializeAppData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const LoadingScreen();
-          } else {
-            return const HomeScreen();
-          }
-        },
-      ),
+      home: const HomeScreen(),
     );
-  }
-
-  Future<void> initializeAppData() async {
-    await Future.delayed(const Duration(seconds: 1));
   }
 }
