0a32f75048b95e38f8c5d9fe3a1870a57e775469
ima17 <ima.sudaraka@outlook.com>
2023-04-16 23:41:32 +0530

    Refactor home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ar_smart_whiteboard/screens/student_screen.dart';
 import 'package:ar_smart_whiteboard/screens/teachers_screen.dart';
+import 'package:ar_smart_whiteboard/widgets/main_button_widged.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,7 +13,8 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
+            MainButton(
+              label: 'Student',
               onPressed: () {
                 Navigator.push(
                   context,
@@ -20,57 +22,16 @@ class HomeScreen extends StatelessWidget {
                       builder: (context) => const StudentScreen()),
                 );
               },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  Colors.blue,
-                ),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                ),
-                padding: MaterialStateProperty.all<EdgeInsets>(
-                  const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                ),
-                elevation: MaterialStateProperty.all<double>(5),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(15.0),
-                child: Text(
-                  'Student',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
+            MainButton(
+              label: 'Teacher',
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => TeachersScreen()),
                 );
               },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  Colors.orange,
-                ),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                ),
-                padding: MaterialStateProperty.all<EdgeInsets>(
-                  const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                ),
-                elevation: MaterialStateProperty.all<double>(5),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(15.0),
-                child: Text(
-                  'Teacher',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
             ),
           ],
         ),
