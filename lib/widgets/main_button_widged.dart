110f0aa6e4cebf7267012fb22efb9668e44dee2b
ima17 <ima.sudaraka@outlook.com>
2023-04-16 23:41:03 +0530

    Create main button widget

---
+++ lib/widgets/main_button_widged.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MainButton extends StatelessWidget {
+  final Function onPressed;
+  final String label;
+  const MainButton({Key? key, required this.onPressed, required this.label})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => onPressed()),
+        );
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(
+          Colors.orange,
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+        padding: MaterialStateProperty.all<EdgeInsets>(
+          const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+        ),
+        elevation: MaterialStateProperty.all<double>(5),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          label,
+          style: const TextStyle(fontSize: 20.0),
+        ),
+      ),
+    );
+  }
+}
